99f0382e5f7fcf4034a3108aa2841c09a788a3ea
Antonio Vieira <vieira.n55@gmail.com>
2023-02-09 11:43:37 -0300

    curso finalizado

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://alura:adevo159118@cluster0.bbho8ne.mongodb.net/alura-node");
+mongoose.connect("mongodb://127.0.0.1:27017/alura-node");
 // mongoose.connect("mongodb+srv://alura:<adevo159118>@cluster0.bbho8ne.mongodb.net/?retryWrites=true&w=majority");
 
 let db = mongoose.connection;
